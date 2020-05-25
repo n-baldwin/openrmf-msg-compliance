8bfeacd07875aac98dab8b23d38dccd2b717e744
n-baldwin <nick.baldwin@cingulara.com>
2020-05-25 05:52:36 -0400

    Tests

--- tests/Models/CciItemTests.cs
+++ tests/Models/CciItemTests.cs
@@ -9,100 +9,99 @@ namespace tests.Models
         [Fact]
         public void Test_NewCCIItemIsValid()
         {
-            CciItem cci = new CciItem();
-            Assert.True(cci != null);
+            CciItem cciItem = new CciItem();
+
+            // Testing
+            Assert.False(cciItem == null);
         }
     
         [Fact]
         public void Test_CCIItemWithDataIsValid()
         {
-            CciItem cci = new CciItem();
+            CciItem cciItem = new CciItem();
             
-            cci.cciId  = "CCI-2345";
-            cci.status = "myStatus";
-            cci.publishDate = DateTime.Now.ToShortDateString();
-            cci.contributor = "Cingulara";
-            cci.definition = "This is my definition";
-            cci.type = "thisType";
-            cci.parameter = "myParams";
-            cci.note = "man this is complicated!";
+            cciItem.cciId  = "CCI-2345";
+            cciItem.status = "myStatus";
+            cciItem.publishDate = DateTime.Now.ToShortDateString();
+            cciItem.contributor = "Cingulara";
+            cciItem.definition = "This is my definition";
+            cciItem.type = "thisType";
+            cciItem.parameter = "myParams";
+            cciItem.note = "man this is complicated!";
 
-            // test things out
-            Assert.True(cci != null);
-            Assert.True (!string.IsNullOrEmpty(cci.cciId));
-            Assert.True (!string.IsNullOrEmpty(cci.status));
-            Assert.True (!string.IsNullOrEmpty(cci.publishDate));
-            Assert.True (!string.IsNullOrEmpty(cci.contributor));
-            Assert.True (!string.IsNullOrEmpty(cci.definition));
-            Assert.True (!string.IsNullOrEmpty(cci.type));
-            Assert.True (!string.IsNullOrEmpty(cci.parameter));
-            Assert.True (!string.IsNullOrEmpty(cci.note));
-            Assert.True (cci.references != null);
-            Assert.True (cci.references.Count == 0);            
+            // Testing
+            Assert.True(cciItem.cciId == "CCI-2345");
+            Assert.True(cciItem.status == "myStatus");
+            Assert.True(cciItem.contributor == "Cingulara");
+            Assert.True(cciItem.definition == "This is my definition");
+            Assert.True(cciItem.type == "thisType");
+            Assert.True(cciItem.parameter == "myParams");
+            Assert.True(cciItem.note == "man this is complicated!");
+            Assert.False(cciItem.references == null);
+            Assert.False(cciItem.publishDate == null);
         }
 
         [Fact]
         public void Test_CCIItemWithReferenceDataIsValid()
         {
-            CciItem cci = new CciItem();
+            CciItem cciItem = new CciItem();
             
-            cci.cciId  = "CCI-2345";
-            cci.status = "myStatus";
-            cci.publishDate = DateTime.Now.ToShortDateString();
-            cci.contributor = "Cingulara";
-            cci.definition = "This is my definition";
-            cci.type = "thisType";
-            cci.parameter = "myParams";
-            cci.note = "man this is complicated!";
+            cciItem.cciId  = "CCI-2345";
+            cciItem.status = "myStatus";
+            cciItem.publishDate = DateTime.Now.ToShortDateString();
+            cciItem.contributor = "Cingulara";
+            cciItem.definition = "This is my definition";
+            cciItem.type = "thisType";
+            cciItem.parameter = "myParams";
+            cciItem.note = "man this is complicated!";
 
-            CciReference cciRef = new CciReference();
+            CciReference cciReference = new CciReference();
             
-            cciRef.creator  = "NIST";
-            cciRef.title = "This is my title here";
-            cciRef.location = "My location";
-            cciRef.index = "AU-9 1.a(2)";
-            cci.references.Add(cciRef);
+            cciReference.creator  = "NIST";
+            cciReference.title = "This is my title here";
+            cciReference.location = "My location";
+            cciReference.index = "AU-9 1.a(2)";
+            cciItem.references.Add(cciReference);
 
-            // test things out
-            Assert.True(cci != null);
-            Assert.True (!string.IsNullOrEmpty(cci.cciId));
-            Assert.True (!string.IsNullOrEmpty(cci.status));
-            Assert.True (!string.IsNullOrEmpty(cci.publishDate));
-            Assert.True (!string.IsNullOrEmpty(cci.contributor));
-            Assert.True (!string.IsNullOrEmpty(cci.definition));
-            Assert.True (!string.IsNullOrEmpty(cci.type));
-            Assert.True (!string.IsNullOrEmpty(cci.parameter));
-            Assert.True (!string.IsNullOrEmpty(cci.note));
-            Assert.True (cci.references != null);
-            Assert.True (cci.references.Count == 1);            
+            // Testing
+            Assert.True(cciItem.cciId == "CCI-2345");
+            Assert.True(cciItem.status == "myStatus");
+            Assert.True(cciItem.contributor == "Cingulara");
+            Assert.True(cciItem.definition == "This is my definition");
+            Assert.True(cciItem.type == "thisType");
+            Assert.True(cciItem.parameter == "myParams");
+            Assert.True(cciItem.note == "man this is complicated!");
+            Assert.True(cciItem.references.Count == 1);
+            Assert.False(cciItem.publishDate == null);
         }
 
 
         [Fact]
         public void Test_NewCCIReferenceIsValid()
         {
-            CciReference cci = new CciReference();
-            Assert.True(cci != null);
+            CciReference cciReference = new CciReference();
+
+            // Testing
+            Assert.False(cciReference == null);
         }
     
         [Fact]
         public void Test_CCIReferenceWithDataIsValid()
         {
-            CciReference cci = new CciReference();
+            CciReference cciReference = new CciReference();
             
-            cci.creator  = "NIST";
-            cci.title = "This is my title here";
-            cci.location = "My location";
-            cci.index = "AU-9 1.a(2)";
-            cci.majorControl = "AU-9";
+            cciReference.creator = "NIST";
+            cciReference.title = "This is my title here";
+            cciReference.location = "My location";
+            cciReference.index = "AU-9 1.a(2)";
+            cciReference.majorControl = "AU-9";
 
-            // test things out
-            Assert.True(cci != null);
-            Assert.True (!string.IsNullOrEmpty(cci.creator));
-            Assert.True (!string.IsNullOrEmpty(cci.title));
-            Assert.True (!string.IsNullOrEmpty(cci.location));
-            Assert.True (!string.IsNullOrEmpty(cci.index));
-            Assert.True (!string.IsNullOrEmpty(cci.majorControl));
+            // Testing
+            Assert.True(cciReference.creator == "NIST");
+            Assert.True(cciReference.title == "This is my title here");
+            Assert.True(cciReference.location == "My location");
+            Assert.True(cciReference.index == "AU-9 1.a(2)");
+            Assert.True(cciReference.majorControl == "AU-9");
         }
     }
 }
